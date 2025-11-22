9b7a2696c8b4b2752cf9d45755ce7d12b63a40da
James Freed <jamesfreed1994@hotmail.com>
2025-11-22 07:45:30 +0000

    Refactor StringCalculator to handle test cases for step 1

--- code-kata/src/main/java/com/jfreed/kata/StringCalculator.java
+++ code-kata/src/main/java/com/jfreed/kata/StringCalculator.java
@@ -1,5 +1,7 @@
 package com.jfreed.kata;
 
+import java.util.Arrays;
+
 public class StringCalculator
 {
     private StringCalculator()
@@ -13,7 +15,15 @@ public class StringCalculator
 
     public int add(String input)
     {
-        return 0;
+        if (input.isEmpty())
+        {
+            return 0;
+        }
+
+        return Arrays.stream(input.split(","))
+                .map(Integer::valueOf)
+                .mapToInt(Integer::intValue)
+                .sum();
     }
 
     private static class SingletonHelper
